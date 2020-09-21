ac9627b8e7dda1d416432f2424992f3a708bb2ad
Robin-Neuman <robinneuman@hotmail.com>
2020-09-21 15:51:44 +0200

    added addOrder and getOrders models

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,3 +1,22 @@
-const { Orders } = require('../database/database')
+const { Order } = require('../database/database')
 const bcrypt = require('bcryptjs')
 require('dotenv').config()
+
+module.exports = {
+    addOrder: async (order) => {
+        try {
+            const query = await Order.create(order)
+            return query
+        } catch (err) {
+            return err
+        }
+    },
+    getOrders: async () => {
+        try {
+            const query = await Order.find()
+            return query
+        } catch (err) {
+            return err
+        }
+    }
+}
--- test/Unittests/orders.test.js
+++ test/Unittests/orders.test.js
@@ -7,15 +7,20 @@ const authenticationModel = require('../../models/authenticationModel')
 
 describe('Test order models', () => {
     it('Should add an order with products to DB', async () => {
-        let response = await orderModel.addOrder()
-        expect(response).to.be.an('object')
-        expect(response.nModified).to.equal(1)
+        const order = {
+            timeStamp: Date.now(), 
+            status: 'inProcess',
+            items: [],
+            orderValue: 999,
+        }
+        let response = await orderModel.addOrder(order)
+        expect(response._doc).to.be.an('object')
+        expect(response._doc).to.have.keys('_id', 'timeStamp', 'status', 'items', 'orderValue', '__v')
     })
 
-    it('Should get all orders', () => {
+    it('Should get all orders', async () => {
         let response = await orderModel.getOrders()
         expect(response).to.be.an('array')
-        expect(response[0]).to.have.keys('_id', 'timeStamp', 'status', 'items', 'orderValue')
+        expect(response[0]._doc).to.have.keys('_id', 'timeStamp', 'status', 'items', 'orderValue', '__v')
     })
 })
-
