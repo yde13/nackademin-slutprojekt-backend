25464dac3278558be8f5a0c4192934a577afaeb3
Ironman <robinneuman@hotmail.com>
2020-09-23 13:30:24 +0200

    Update orders.test.js

--- test/Integrationstester/orders.test.js
+++ test/Integrationstester/orders.test.js
@@ -1,8 +1,8 @@
 const chai = require('chai')
-chai.should()
+const chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
 const {disconnect, connect} = require('../../database/database')
-const { expect, request } = require('chai')
-const chaiHttp = require('chai-http')
+const expect = chai.expect
 const orderModel = require('../../models/orderModel')
 const { getTestOrders } = require('../testdata')
 const app = require('../../app.js')
@@ -10,17 +10,20 @@ const app = require('../../app.js')
 describe('Integration test for orders', () => {   
     let orders; 
     before(async () => {
-        await db.connect()
+        await connect()
         orders = await getTestOrders()
     })
 
-    it('Should test out api order POST route',  async () => {
-        const query = await request(app)
-            .post('/orders')
-            .set()
+    it('Should test out api order POST route without being logged in',  async () => {
+        const query = await chai.request(app)
+            .post('/api/orders')
+            .set('Authorization', `Bearer `)
+            .send(orders[0])
+
+            console.log(query)
     })
 
     after(async () => {
-        await db.disconnect()
+        await disconnect()
     })
 })
